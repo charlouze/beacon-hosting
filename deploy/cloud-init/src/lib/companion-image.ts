e9f8fb8f9b9ec1b120be086960e7481c0feee0b1
Charlouze <me@charlouze.com>
2026-09-08 14:37:15 +0200

    build(cloud-init): epingle le compagnon qui sait restaurer deux sources

--- deploy/cloud-init/src/lib/companion-image.ts
+++ deploy/cloud-init/src/lib/companion-image.ts
@@ -9,4 +9,4 @@
  * day a new image is published there must be exactly one line to change.
  */
 export const COMPANION_IMAGE =
-  'ghcr.io/charlouze/beacon-companion@sha256:7717f76dcc07185554d7ce26ef13ff042121185460f4722724b56b781005b9ef';
+  'ghcr.io/charlouze/beacon-companion@sha256:4728d2de3e4abc9b7439b25e299e1a70fe86f32ecccb9d98dd6e44debed817ca';
